f59f663338fcb221301826908b131a88eb7f1358
Keifer.Gu <keifergu@gmail.com>
2016-11-21 16:25:11 +0800

    添加AnimalsInfo组件

---
+++ src/components/AnimalInfo.vue
@@ -0,0 +1,19 @@
+<template>
+<div>
+  <h1>{{animal.name}}</h1>
+  <p>生命值：{{animal.health}}</p>
+  <p>攻击力：{{animal.damage}}</p>
+  <p>移动速度：{{animal.speed}}</span>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'animalInfo',
+  props: {
+    animal: {
+      type: Object,
+    },
+  },
+};
+</script>
--- src/components/Animals.vue
+++ src/components/Animals.vue
@@ -1,9 +1,33 @@
 <template>
-  <p>Animals</p>
+<div>
+  <animal-info
+    v-for="animal in allAnimals"
+    :animal="animal"
+    >
+  </animal-info>
+</div>
 </template>
 
 <script>
+
+import AnimalInfo from './AnimalInfo';
+
+const info = [
+  {
+    name: '猪人',
+    health: 250,
+    damage: 33,
+    speed: 3,
+  },
+];
+
 export default {
   name: 'animals',
+  data() {
+    return {
+      allAnimals: info,
+    };
+  },
+  components: { AnimalInfo },
 };
 </script>
